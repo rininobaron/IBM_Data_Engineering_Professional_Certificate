328f90e18ac46fb64ecdf6d4d84b311b879dfd74
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-06-23 15:09:26 -0500

    Update commands.sql in COURSE_9.../Week1/Lab2

--- COURSE_9_Relational_Database_Administration/Week1/Lab2/commands.sql
+++ COURSE_9_Relational_Database_Administration/Week1/Lab2/commands.sql
@@ -34,4 +34,32 @@ INSERT INTO csv_test VALUES(1,'data one'),(2,'data two'),(2,'data three');
 --Show the content in csv_test table
 SELECT * FROM csv_test;
 
+--Exercise 3: Navigate the MySQL System Tables
+--Grant System Table Category
+SHOW DATABASES;
 
+--change to mysql database
+USE mysql;
+
+--view tables
+SHOW TABLES;
+
+--view the User column in user table
+SELECT User FROM user;
+
+--create new user
+CREATE USER test_user;
+
+--Exercise 4: Try it Yourself!
+
+--PROBLEM 1
+USE world;
+
+--PROBLEM 2
+CREATE TABLE MyISAM_test (i INT NOT NULL, c CHAR(10) NOT NULL) ENGINE = MYISAM;
+
+--PROBLEM 3
+USE information_schema;
+
+--PROBLEM 4
+SELECT table_name, engine FROM information_schema.TABLES WHERE table_schema = 'world';
