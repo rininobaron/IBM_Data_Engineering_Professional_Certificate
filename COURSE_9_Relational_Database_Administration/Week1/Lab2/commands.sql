bc122df596ca1191a089403f6cfd84275cf53344
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-06-23 13:16:38 -0500

    Add sql statements from EXERCISE 2

--- COURSE_9_Relational_Database_Administration/Week1/Lab2/commands.sql
+++ COURSE_9_Relational_Database_Administration/Week1/Lab2/commands.sql
@@ -2,6 +2,8 @@
 -- in linux bash
 mysql -u root -p
 
+--Exercise 1: Create your database
+
 --in mysql shell
 --CREATE DATABASE called world
 CREATE DATABASE world;
@@ -14,3 +16,22 @@ SOURCE world_mysql_script.sql;
 
 --Display the tables in database world
 SHOW TABLES;
+
+--Exercise 2: Manage MySQL Storage Engines
+
+--View engines in mysql
+SHOW ENGINES;
+
+--Create a new table with csv engine
+CREATE TABLE csv_test (i INT NOT NULL, c CHAR(10) NOT NULL) ENGINE = CSV;
+
+--confirm
+SHOW TABLES;
+
+--Add ome examples
+INSERT INTO csv_test VALUES(1,'data one'),(2,'data two'),(2,'data three');
+
+--Show the content in csv_test table
+SELECT * FROM csv_test;
+
+
