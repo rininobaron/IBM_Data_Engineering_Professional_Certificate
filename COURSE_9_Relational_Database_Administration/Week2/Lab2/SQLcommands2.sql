9a521f809e1b846c0bf90882f6d57dad82fc28fb
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-06-28 11:41:38 -0500

    Update Lab2 in Week2

--- COURSE_9_Relational_Database_Administration/Week2/Lab2/SQLcommands2.sql
+++ COURSE_9_Relational_Database_Administration/Week2/Lab2/SQLcommands2.sql
@@ -10,6 +10,12 @@ SELECT * FROM aircrafts_data;
 --Insert new row into aircrafts_data 
 INSERT INTO aircrafts_data(aircraft_code, model, range) VALUES (380, '{"en": "Airbus A380-800"}', 15700);
 
-Confirm if the row was inserted correctly
+--Confirm if the row was inserted correctly
 SELECT * FROM aircrafts_data;
+
+--Query
+SELECT * FROM tickets WHERE booking_ref = '0002D8';
+
+--Update name value
+UPDATE tickets SET passenger_name = 'SANYA KORELEVA' WHERE booking_ref = '0002D8';
 
