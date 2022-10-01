7b84d6015b6e709f9a757861028354fbdf5145a2
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-10-01 05:13:42 -0500

    Add solutions for Exercise 1

---
+++ COURSE_9_Relational_Database_Administration/Week3/Lab2/solutions.sql
@@ -0,0 +1,21 @@
+--Exercise 1: Create Your Database
+
+--4. In the Terminal, create a new database called world.
+--EXTRA 
+mysql --host=127.0.0.1 --port=3306 --user=root --password --execute="SHOW DATABASES;"
+--ANSWER
+mysql --host=127.0.0.1 --port=3306 --user=root --password --execute="CREATE DATABASE world;"
+--EXTRA
+mysql --host=127.0.0.1 --port=3306 --user=root --password --execute="SHOW DATABASES;"
+
+--5. Now, we'll want to use the newly created database, world. How can we go about doing that?
+USE world;
+
+--6. In order to complete the database creation process, we'll want to execute the MySQL 
+--script containing the data that we downloaded. This file will create tables and insert 
+--data into those tables.
+SOURCE world_mysql_script.sql;
+
+--7. With our database created, we'll want to take a look at the tables inside it to get 
+--a general idea of the data we have. What tables do we have?
+SHOW TABLES;
