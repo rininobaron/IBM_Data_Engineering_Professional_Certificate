ddfb8a7daad0715ac299b254ae940950fecade13
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-11-08 03:50:11 -0600

    Add first solution

---
+++ COURSE_07_Relational_Database_Administration/Week4/Lab2/solutions.sql
@@ -0,0 +1,12 @@
+--Exercise
+
+--Task A: Create a Database
+
+--9.- Create a new database sakila using 
+--the command below in the terminal 
+--and proceed to Task B:
+
+CREATE DATABASE sakila;
+
+--Task B: Restore the Structure and 
+--Data of a Table
