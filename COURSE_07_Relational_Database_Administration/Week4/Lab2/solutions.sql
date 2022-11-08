6ca980d96079ff1c7d3067738a78a52b8df922ea
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-11-08 04:19:21 -0600

    Update solutions

--- COURSE_07_Relational_Database_Administration/Week4/Lab2/solutions.sql
+++ COURSE_07_Relational_Database_Administration/Week4/Lab2/solutions.sql
@@ -10,3 +10,27 @@ CREATE DATABASE sakila;
 
 --Task B: Restore the Structure and 
 --Data of a Table
+
+--1.- To use the newly created empty sakila 
+--database, use the command below in 
+--the terminal:
+
+use sakila;
+
+--2.- Restore the sakila mysql dump file 
+--(containing the sakila database table 
+--definitions and data) to the newly 
+--created empty sakila database using 
+--the command below in the terminal:
+
+source sakila_mysql_dump.sql;
+
+--3.- To check, list all the table names
+--from the sakila database using the 
+--command below in the terminal
+
+SHOW FULL TABLES WHERE table_type = 'BASE TABLE';
+
+--Task C: Understanding the Process 
+--Involved in Creating MySQL Database 
+--Backups
