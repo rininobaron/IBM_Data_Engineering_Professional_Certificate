2d65eb24b189a3d25645a375a5075ac1e92b9d0f
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-11-28 04:16:07 -0600

    Last solutions Lab2 Week4

--- COURSE_07_Relational_Database_Administration/Week4/Lab2/solutions.sql
+++ COURSE_07_Relational_Database_Administration/Week4/Lab2/solutions.sql
@@ -62,4 +62,131 @@ GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost';
 --If you have any error edit the sqlbackup.sh file adding 
 ----user=userNAme on mysqldump statement
 
+--Practice Exercise
 
+--1.- Change the crontab schedule to create a backup every week 
+--on Monday at 12:00 a.m.
+
+0 0 * * 1 /home/project/sqlbackup.sh > /home/project/backup.log
+
+--Change the crontab schedule to create a backup every day 
+--at 6:00 a.m. 
+
+0 6 * * * /home/project/sqlbackup.sh > /home/project/backup.log
+
+--Task F: Truncate the Tables in the Database
+
+-- 1.- Create a new file named truncate.sh under home/project.
+
+
+--2.-Copy this script and paste it in the new file.
+
+
+--3.-Change the permission of the file by running the following command:
+
+sudo chmod u+x+r truncate.sh
+
+--4.-Execute the script to truncate the tables.
+
+--5.-To check whether the tables in the database are truncated, 
+--log in to the database with the credentials.
+
+mysql
+
+--6.- Switch to the sakila database.
+
+use sakila;
+
+--7.- Check all the tables in the database.
+SHOW TABLES;
+
+--8.- Retrieve all the rows from staff table. 
+--If the truncate was successful, 
+--the output should be an Empty set.
+
+SELECT * FROM staff;
+
+--9.- Quit the mysql prompt.
+
+
+--Task G: Restore the Database
+
+--1.- In the terminal window, run the following command 
+--to find the list of backup files that have been created
+ls -l backups
+
+--2.- Select the file that you want to restore the data from 
+--and copy the file name.
+
+--3.- Unzip the file and extract the SQL file from the backup file.
+unzip all-database-27-11-2022_04-48-01.gz
+
+--4.- Populate and restore the database with the sqlfile that results 
+--from the unzip operation.
+mysql sakila < all-database-27-11-2022_04-48-01
+
+--Check the table
+
+
+--Practice Exercise
+
+--1.- Create a shell script which takes the database name and back up 
+--directory as parameters and backsup the database as <dbname>_
+--timestamp.sql in the backup directory. If the database 
+--doesn't exist, it should display appropriate message. 
+--If the backup dir doesn't exist, it should create one.
+
+dbname=$(mysql -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$1'" | grep $1)
+
+if [ ! -d $2 ]; then 
+    mkdir $2
+fi
+
+if [ $1 == $dbname ]; then
+    sqlfile=$2/$1-$(date +%d-%m-%Y).sql
+    if mysqldump  $1 > $sqlfile ; then
+    echo 'Sql dump created'
+    else
+        echo 'Error creating backup!'
+    fi
+else
+    echo "Database doesn't exist"
+fi
+
+--Linux command
+
+./answer1 employees backups 
+
+--2.- Write a shell script which takes the database name and 
+--the script file as parameters and restores the database 
+--from the sql file.
+
+sqlfile=backups/$2
+
+if [ -f $sqlfile ]; then 
+
+    dbname=$(mysql -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$1'" | grep $1)
+    
+    if test -z "$dbname"; then
+        dbname='_'
+    fi
+
+    if [ $1 != $dbname ]; then
+        echo "Created DB as it didn't exist"
+        mysql -e "CREATE DATABASE $1"
+    fi
+
+    if mysql $1 < $sqlfile ; then
+        echo 'Successful restoring database'
+    else
+        echo 'Error restoring database!'
+    fi
+else
+    echo "File doesn't exist"
+fi
+
+--Linux commands
+
+chmod u+x answer2.sh
+
+./answer2.sh employees 'employees-28-11-2022.sql'
