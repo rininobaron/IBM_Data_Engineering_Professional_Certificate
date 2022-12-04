e775c948301203714008fe6c9e044348600bb18a
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-12-04 05:03:00 -0600

    Add final solutions to first airflow lab

---
+++ COURSE_08_ETL_and_Data_Pipelines_with_Shell_Airflow_and_Kafka/Week3/Lab1/Practice_Exercises.sh
@@ -0,0 +1,12 @@
+# Problem 1
+airflow tasks list example_branch_labels
+
+# Problem 2
+airflow dags unpause example_branch_labels
+
+# Sleep 2 minutes
+echo "Wait two minutes, please!"
+sleep 120
+
+# Problem 3
+airflow dags pause example_branch_labels
