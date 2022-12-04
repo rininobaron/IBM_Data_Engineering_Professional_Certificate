b5a507cf065f04b1f4657a0b5054cc0554e94538
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-12-04 05:01:52 -0600

    Add bash file in order to start airflow

---
+++ COURSE_08_ETL_and_Data_Pipelines_with_Shell_Airflow_and_Kafka/Week3/Lab1/airflow_start_ubuntu.sh
@@ -0,0 +1,13 @@
+# install from pypi using pip
+#pip install apache-airflow
+
+# initialize the database
+airflow db init
+
+# start the web server, default port is 8080
+airflow webserver -p 8080
+
+# start the scheduler
+airflow scheduler
+
+# visit localhost:8080 in the browser and enable the example dag in the home page
