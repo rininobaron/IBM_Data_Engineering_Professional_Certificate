99826f23a247c3ccdcdbc39b3c4a6fff50145e9c
Ricardo Nino de Rivera <rininobaron@gmail.com>
2022-12-06 05:43:03 -0600

    Update file

--- COURSE_08_ETL_and_Data_Pipelines_with_Shell_Airflow_and_Kafka/Week3/Lab1/airflow_start_ubuntu.sh
+++ COURSE_08_ETL_and_Data_Pipelines_with_Shell_Airflow_and_Kafka/Week3/Lab1/airflow_start_ubuntu.sh
@@ -7,7 +7,4 @@ airflow db init
 # start the web server, default port is 8080
 airflow webserver -p 8080
 
-# start the scheduler
-airflow scheduler
-
 # visit localhost:8080 in the browser and enable the example dag in the home page
