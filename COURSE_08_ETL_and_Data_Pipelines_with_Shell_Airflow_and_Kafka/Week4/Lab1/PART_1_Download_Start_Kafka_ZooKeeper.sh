25c1da98008e550cafff43c75a64a64b6c34020f
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-01-11 16:54:49 -0600

    Add bash script to download and start kafka and zookeeper

---
+++ COURSE_08_ETL_and_Data_Pipelines_with_Shell_Airflow_and_Kafka/Week4/Lab1/PART_1_Download_Start_Kafka_ZooKeeper.sh
@@ -0,0 +1,49 @@
+# Exercise 1 - Download and extract Kafka
+
+LINK="https://archive.apache.org/dist/kafka/2.8.0/kafka_2.12-2.8.0.tgz"
+
+# Check if Kafka tgz file is downloaded
+echo "Check if Kafka is downloaded"
+KAFKA=""
+for file in *;
+do
+	if [ ${file:0:5} == "kafka" ] && [ ${file:0-4} == ".tgz" ]
+	then
+		# Save kafka version name
+		KAFKA=${file:0:-4}
+		# Extract Kafka
+		echo "Extract Kafka file"
+		tar -xzf $file
+		rm -rf $file
+		echo $file" IS DELETED"
+		echo "VERSION "$KAFKA
+		break
+	fi
+done
+# If there is NOT Kafka tgz file
+if [[ $KAFKA == "" ]]
+then
+	echo "Download Kafka tgz file"
+	wget $LINK
+	for file in *;
+	do
+		if [ ${file:0:5} == "kafka" ] && [ ${file:0-4} == ".tgz" ]
+		then
+			# Save kafka version name
+			KAFKA=${file:0:-4}
+			# Extract Kafka
+			echo "Extract Kafka file"
+			tar -xzf $file
+			rm -rf $file
+			echo $file" IS DELETED"
+			echo "VERSION "$KAFKA
+			break
+			fi
+	done
+fi
+
+# Exercise 2 - start ZooKeeper
+
+# Start the ZooKeeper server
+echo "Start the ZooKeeper server"
+$KAFKA/bin/zookeeper-server-start.sh $KAFKA/config/zookeeper.properties
