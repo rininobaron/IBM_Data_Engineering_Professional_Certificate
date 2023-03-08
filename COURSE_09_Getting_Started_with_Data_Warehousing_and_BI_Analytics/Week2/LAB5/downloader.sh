bf20f48d293cef1f20e06a63635720d731377d28
Ricardo Nino de Rivera <rininobaron@gmail.com>
2023-03-07 18:21:10 -0600

    Add script downloader, download necessary files

---
+++ COURSE_09_Getting_Started_with_Data_Warehousing_and_BI_Analytics/Week2/LAB5/downloader.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+echo "Dwonalod necessary files"
+echo ""
+
+wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0260EN-SkillsNetwork/labs/Populating%20a%20Data%20Warehouse/star-schema.sql
+
+wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0260EN-SkillsNetwork/labs/Populating%20a%20Data%20Warehouse/DimCustomer.sql
+
+wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0260EN-SkillsNetwork/labs/Populating%20a%20Data%20Warehouse/DimMonth.sql
+
+wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0260EN-SkillsNetwork/labs/Populating%20a%20Data%20Warehouse/FactBilling.sql
